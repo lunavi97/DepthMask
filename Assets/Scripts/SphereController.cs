66a47449210aab988d1264f5c0ddd332e836b831
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-13 03:41:31 -0300

    Redesigned objects, modified directional light and added movement

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    };
+    public float acceleration;
+    
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        int num = (int)Random.Range((int)Direction.Up, (int)Direction.Left + 1);
+        
+        if (num == (int)Direction.Up) {
+            rb.AddForce(0, 0, acceleration);
+        }
+
+        else if (num == (int)Direction.Right)
+        {
+            rb.AddForce(acceleration, 0, 0);
+        }
+
+        else if (num == (int)Direction.Down)
+        {
+            rb.AddForce(0, 0, -acceleration);
+        }
+
+        else
+        {
+            rb.AddForce(-acceleration, 0, 0);
+        }
+	}
+}
